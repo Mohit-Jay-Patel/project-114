a33141c0e3e0aefb4af18b25cd71d78efe56bb54
Mohit-Jay-Patel <113121401+Mohit-Jay-Patel@users.noreply.github.com>
2023-04-10 19:40:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var video="";
-function preload(){
+var noseX=0;
+var noseY=0;
+var clown_nose="";
 
+function preload(){
+clown_nose=loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 function setup(){
 var canvas=createCanvas(350,350);
@@ -16,6 +20,7 @@ function modalLoaded(){
 }
 function draw(){
 image(video,0,0,350,350);
+image(clown_nose,noseX-15,noseY+7,30,30);
 }
 function take_snapshot(){
     save("Photo.png");
@@ -23,6 +28,8 @@ function take_snapshot(){
 function got_poses(results){
     if(results.length>0){
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
         console.log("nose x = "+results[0].pose.nose.x);
         console.log("nose y = "+results[0].pose.nose.y);
     }
